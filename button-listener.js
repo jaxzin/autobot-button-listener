2eb6aca092de05e2639d23b58703101a84397581
Brian Jackson <brj@apple.com>
2015-09-04 00:28:32 -0700

    Initial commit.

---
+++ button-listener.js
@@ -0,0 +1,30 @@
+var BlendMicro = require('blendmicro');
+
+var bm = new BlendMicro("Adafruit Bluefruit LE");
+
+bm.on('open', function(){
+  console.log("open!!");
+
+  // read data
+  bm.on("data", function(data){
+    console.log(data.toString());
+    bm.updateRssi(function(err, rssi){
+      console.log("rssi:"+rssi);
+    });
+  });
+
+});
+
+bm.on('close', function(){
+  console.log('close!!');
+});
+
+process.stdin.setEncoding("utf8");
+
+// write data from STDIN
+process.stdin.on("readable", function(){
+  var chunk = process.stdin.read();
+  if(chunk == null) return;
+  chunk = chunk.toString().replace(/[\r\n]/g, '');
+  bm.write(chunk);
+});
